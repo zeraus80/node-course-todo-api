837e87b308203e57d44fed68ce04d88d3b3cefab
Javier Navarro <jnavarros@psl.com.co>
2018-03-29 19:48:40 -0500

    Add GET /todos

--- server/server.js
+++ server/server.js
@@ -17,6 +17,10 @@ app.post('/todos', (req, res) => {
     todo.save().then(doc => res.send(doc), err => res.status(400).send(err));
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then(todos => res.send({todos}), e => res.status(400).send(e));
+});
+
 app.listen(3000, () => {
     console.log('started on port 3000');
 }); 
