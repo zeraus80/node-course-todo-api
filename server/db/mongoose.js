663f17a9669f91891c58aa6ebcf1ed0692cf3579
Javier Navarro <jnavarros@psl.com.co>
2018-04-09 20:48:05 -0500

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,21 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+var connectPath, options;
+//Check if we are on Heroku
+if(process.env.PORT){
+ connectPath = "mongodb://<dbuser>:<dbpassword>@ds241019.mlab.com:41019/javier-todo";
+ options= {
+     auth: {
+         user: 'jnavarro',
+         password: 'zuca6419'
+     }
+ }
+}else{
+ connectPath = "mongodb://localhost:27017/TodoApp";
+ options = {}
+}
+mongoose.connect(connectPath, options);
 
 module.exports = { mongoose };
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -34,8 +35,8 @@ app.get('/todos/:id', (req, res) => {
         .catch(e => res.status(400).send());
 });
 
-app.listen(3000, () => {
-    console.log('started on port 3000');
+app.listen(port, () => {
+    console.log(`Started up at port ${port}`);
 }); 
 
 module.exports = { app };
