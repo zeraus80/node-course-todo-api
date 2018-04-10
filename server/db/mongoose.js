32935a2e0589a6c00a16838fa0d3a0d456093a61
Javier Navarro <jnavarros@psl.com.co>
2018-04-09 21:14:47 -0500

    Setup heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,18 +4,19 @@ mongoose.Promise = global.Promise;
 
 var connectPath, options;
 //Check if we are on Heroku
-if(process.env.PORT){
- connectPath = 'mongodb://<dbuser>@ds241019.mlab.com:41019/javier-todo';
- options= {
-     auth: {
-         user: 'jnavarro',
-         password: 'zuca6419'
-     }
- }
-}else{
- connectPath = 'mongodb://localhost:27017/TodoApp';
- options = {}
+if (process.env.PORT) {
+    connectPath = `mongodb://<dbuser>@ds241019.mlab.com:41019/javier-todo`;
+    options = {
+        auth: {
+            user: 'jnavarro',
+            password: 'zuca6419'
+        }
+    };
+} else {
+    connectPath = 'mongodb://localhost:27017/TodoApp';
+    options = {};
 }
+
 mongoose.connect(connectPath, options);
 
 module.exports = { mongoose };
