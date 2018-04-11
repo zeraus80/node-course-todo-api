67d23b8bb43cf2551c83e6903ca79d981fb2d8c2
Javier Navarro <jnavarros@psl.com.co>
2018-04-10 20:06:02 -0500

    Adding DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -35,6 +35,18 @@ app.get('/todos/:id', (req, res) => {
         .catch(e => res.status(400).send());
 });
 
+app.delete('/todos/:id', (req, res) => {
+    const { id } = req.params;
+    
+    if (!ObjectId.isValid(id)) {
+        res.status(404).send();
+    }
+
+    Todo.findByIdAndRemove(id)
+        .then(doc => doc ? res.send({ doc }) : res.status(404).send())
+        .catch(e => res.status(400).send());
+});
+
 app.listen(port, () => {
     console.log(`Started up at port ${port}`);
 }); 
