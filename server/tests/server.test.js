39d883cb9fb8188cd3adbac8b12fbb84adf5728e
Javier Navarro <jnavarros@psl.com.co>
2018-04-11 20:45:41 -0500

    PATCH /todos/:id test

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -10,7 +10,9 @@ const todos = [{
     text: 'clean the room'
 }, {
     _id: new ObjectID(),
-    text: 'go tho the gym'
+    text: 'go tho the gym',
+    completed: true,
+    completedAt: 333
 }];
 
 
@@ -130,5 +132,43 @@ describe('DELETE /todos/:id', () => {
         .delete('/todos/123')
         .expect(404)
         .end(done);
+    });
+});
+
+describe('PATCH /todos/:id', () => {
+    it('should update the todo', done => {
+        var hexId = todos[0]._id.toHexString();
+        const payload = {
+            text: 'clean desk',
+            completed: true
+        };
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send(payload)
+            .expect(200)
+            .expect(res => {
+                expect(res.body.todo.text).toBe(payload.text);
+                expect(res.body.todo.completed).toBeTruthy();                
+            })
+            .end(done);        
+    });
+
+    it('should clear completedAt when todo is not completed', done => {
+        var hexId = todos[1]._id.toHexString();
+        const payload = {
+            text: 'clean bathroom',
+            completed: false
+        };        
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send(payload)
+            .expect(200)
+            .expect(res => {
+                expect(res.body.todo.text).toBe(payload.text);
+                expect(res.body.todo.completed).toBeFalsy();                
+                expect(res.body.todo.completedAt).toBeNull();                
+            })
+            .end(done);  
+
     });
 });
