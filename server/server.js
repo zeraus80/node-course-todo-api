780c23c9898bec013f95d25ed21df262aceaac41
Javier Navarro <jnavarros@psl.com.co>
2018-04-17 18:27:14 -0500

    Add token method on user object

--- server/server.js
+++ server/server.js
@@ -70,6 +70,17 @@ app.patch('/todos/:id', (req, res) => {
         .catch(e => res.status(400).send());
 });
 
+app.post('/users', (req, res) => {
+    const body = _.pick(req.body, ['email', 'password']);
+
+    var user = new User(body);
+    
+    user.save()
+        .then(() => user.generateAuthToken())
+        .then(token => res.header('x-auth', token).send(user))
+        .catch(err => res.status(400).send(err));
+});
+
 app.listen(port, () => {
     console.log(`Started up at port ${port}`);
 }); 
