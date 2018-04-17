b063f73d3f4ce12caa05721c2cc2d127fe8ea52c
Javier Navarro <jnavarros@psl.com.co>
2018-04-17 18:27:36 -0500

    Add token method on user object

---
+++ playground/hashing.js
@@ -0,0 +1,8 @@
+const { SHA256 } = require('crypto-js');
+const { jws } = require('jsonwebtoken');
+
+var message = 'I am user number 3';
+var hash = SHA256(message).toString();
+
+console.log(`Message: ${message}`);
+console.log(`Hash: ${hash}`);
