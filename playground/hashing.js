6650f959e31542fcb6d9e285cf1a5b3d757099ea
Javier Navarro <jnavarros@psl.com.co>
2018-04-19 19:47:50 -0500

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,8 +1,4 @@
 const { SHA256 } = require('crypto-js');
 const { jws } = require('jsonwebtoken');
 
-var message = 'I am user number 3';
-var hash = SHA256(message).toString();
-
-console.log(`Message: ${message}`);
-console.log(`Hash: ${hash}`);
+const bcrypt = require('bcryptjs');
--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,4 +1,4 @@
-const {ObjectId} = require('mongodb');
+const { ObjectId } = require('mongodb');
 
 const { mongoose } = require('./../server/db/mongoose');
 const { Todo } = require('./../server/models/todo');
