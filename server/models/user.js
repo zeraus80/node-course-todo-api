710da33c331e2cd556402e09f814c3842853c1d0
Javier Navarro <jnavarros@psl.com.co>
2018-04-23 20:11:43 -0500

    add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -67,6 +67,20 @@ UserSchema.statics.findByToken = function (token) {
     });
 };
 
+UserSchema.statics.findByCredentials = function ({email, password}) {
+    var User = this;
+    return User.findOne({email}).then(user => {
+        if (!user) { return Promise.reject(); }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, result) => {
+                if (result) { resolve(user); }
+                else { reject(err); }
+            })
+        });
+    });
+};
+
 UserSchema.pre('save', function (next) {
     var user = this;
     if (user.isModified('password')) {        
