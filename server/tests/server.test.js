604d724624c0cd01ce87d59e708ce8efe2f0dc99
Javier Navarro <jnavarros@psl.com.co>
2018-05-01 10:46:56 -0500

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -281,3 +281,20 @@ describe('POST /users/login', () => {
             });
     });
 });
+
+describe('DELETE /users/me/token', () => {
+    it('should remove auth token on logout', done => {
+        const {_id} = users[0];
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', users[0].tokens[0].token)
+            .expect(200)
+            .end((err, res) => {
+                if (err) return done(err);
+                User.findById(_id).then(user => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch(e => done(e));
+            });
+    });
+});
