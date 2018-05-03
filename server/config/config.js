84e3258e7d9f7d4e3367e28e0dc1256cf36131ea
Javier Navarro <jnavarros@psl.com.co>
2018-05-02 20:06:19 -0500

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,12 @@
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
+if (env === 'development' || env === 'test') {
+    var config = require('./config.json');
+    var envConfig = config[env];
+    Object.keys(envConfig).forEach(key => process.env[key] = envConfig[key]);
+}
+
+/* if (env === 'development') {
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
 } else if (env === 'test') {
@@ -8,4 +14,4 @@ if (env === 'development') {
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
 } else if (env === 'production') {
     process.env.MONGODB_URI = 'mongodb://ds241019.mlab.com:41019/javier-todo';
-}
+} */
